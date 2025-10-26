30ffa7486e31143117de59b2ffddb8e28e7df9be
Sierra07-w <mukhammadsara07@gmail.com>
2025-10-26 15:47:13 +0500

    Add MSTResult class with properties and constructor

--- src/main/java/org/example/MSTResult.java
+++ src/main/java/org/example/MSTResult.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.List;
+
 public class MSTResult {
+    public List<Edge> mst_edges;
+    public double total_cost;
+    public int operations_count;
+    public double execution_time_ms;
+
+    public MSTResult(List<Edge> mst_edges, double total_cost, int operations_count, double execution_time_ms) {
+        this.mst_edges = mst_edges;
+        this.total_cost = total_cost;
+        this.operations_count = operations_count;
+        this.execution_time_ms = execution_time_ms;
+    }
 }
