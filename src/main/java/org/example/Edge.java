ce95fe5d4c4842ba352d472af85bc6103cce4a9e
Sierra07-w <mukhammadsara07@gmail.com>
2025-10-26 15:43:33 +0500

    Add Edge

--- src/main/java/org/example/Edge.java
+++ src/main/java/org/example/Edge.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class Edge {
+    public String from;
+    public String to;
+    public double weight;
+
+    public Edge(String from, String to, double weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + from + " - " + to + " : " + weight + ")";
+    }
 }
